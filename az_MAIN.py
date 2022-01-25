7397fae91cde47689e12dd9414503a31f8a1f0d4
Tiep M. H <50709837+TiepMH@users.noreply.github.com>
2022-01-25 18:58:50 +0000

    Add files via upload

---
+++ az_MAIN.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: TIEP M. HOANG
+
+@email: hmt1803@gmail.com
+
+"""
+
+'''
+You have to set up the system parameters in 'class_SysParam.py' first
+
+The following commands will then work perfectly
+'''
+
+# exec(open('az_p1_generate_data.py').read())
+
+# exec(open('az_p2_ETL.py').read())
+
+# exec(open('az_p3_learning.py').read())
+
+# exec(open('az_p4_illustration.py').read())
+
+exec(open('az_p3_learning_WRITING_PAPER.py').read())
+
+exec(open('az_p4_illustration_WRITING_PAPER.py').read())
